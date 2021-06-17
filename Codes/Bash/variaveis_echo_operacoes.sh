6f5977a830d751093aacbee15d678353180cc7ef
vitinsec <vitoridesec@gmail.com>
2021-06-17 06:39:55 -0300

    Upload projeto Inicial

---
+++ Codes/Bash/variaveis_echo_operacoes.sh
@@ -0,0 +1,19 @@
+#Todo conteudo de aprendizado deve ser disponibilizado de forma clara e com subtrehos de códigos
+'''
+    O Bash apresenta a extensão .sh como padrão para seus programas, sendo necessário validar sua execução(caso ainda não tenha)
+por meio do comando "chmod +x < arq.sh >" 
+
+'''
+#Indicar lib de Linux para entender diretórios e permissões
+
+#Os arquivos no linux podem começar indicando qual o binário responsável por sua execução, no caso/bin/bash
+# O comando echo é responsável por imprimir na tela, podendo ser acompanhado por aspas simples ou duplas.
+# No entando o comando echo acompanhado por uma variável deve vir acompanhado por aspas duplas.
+
+
+#!/bin/bash
+echo "Olá mundo!"
+echo "Sou um arquivo de texto sendo executado pelo binário /bin/bash"
+num = 2
+texto = "texto"
+type $num
